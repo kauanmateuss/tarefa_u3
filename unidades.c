3328be54ab14f05c9500121b8e708b1b3c3d2022
kauanmateuss <kamateus77@gmail.com>
2024-12-21 13:51:18 -0300

    Adicionei os arquivos unidade.c e unidade.h

---
+++ unidades.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "unidades.h"   // Incluindo a bibliotecas que contem os protótipos das funções
+
+// AQUI PRA BAIXO VOCÊ PODE COLOCAR OS CORPOS (BLOCOS) DAS FUNÇÕES QUE DECLARARAM NO UNIDADES.H
+
+// EX:
+
+/* 
+void escrever(){
+
+    printf("Escrevi na tela\n");
+
+}
+
+
+*/
