96aa5ca0a40795253fe40b173bdd8e92b776b1a1
kauanmateuss <kamateus77@gmail.com>
2024-12-21 13:52:17 -0300

    Adicionei o menu de escolha do usuário

--- main.c
+++ main.c
@@ -1,7 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "unidades.h"   // incluindo a biblioteca com as funções que vocês desenvolveram.
+
 
 int main() {
-    
+    int opcao;
+
+    printf("====================== BEM VINDO AO CONVERSOR DE UNIDADES ==========================\n");
+
+    // Perguntando ao usuario qual opcao ele quer executar:
+
+    do{
+        printf("1 - COMPRIMENTO\n2 - MASSA\n3 - VOLUME\n4 - TEMPERATURA\n5 - TEMPO\nQUAL CONVERSOR DESEJA EXECUTAR: ");
+        scanf("%d", &opcao);
+
+        // switch para multiplas escolhas:
+        switch(opcao){
+            case 0:
+                printf("SAINDO DO PROGRAMA...\n");
+                break;
+            
+            case 1:
+                //chamar a função unidades de COMPRIMENTO;
+                break;
+
+            case 2:
+                //chamar a função de unidades de MASSA;
+                break;
+
+            case 3:
+                // chamar a função de unidades de VOLUME;
+                break;
+
+            case 4:
+                // chamar a função de unidades de TEMPERATURA;
+                break;
+            
+            case 5:
+                // chamar a função de unidades de TEMPO
+                break;
+            
+            default:
+                printf("OPCAO INVALIDA. TENTE NOVAMENTE\n");
+                continue;
+
+        }
+
+    }while(opcao != 0);
+
 
     return 0;
 }
